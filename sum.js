4e42c607e677cf59a91af26cf28c52d5533113a5
Echelon0815 <jack.w886@gmail.com>
2022-12-03 16:08:23 +0800

    調換floor&abs

--- sum.js
+++ sum.js
@@ -1,5 +1,5 @@
 function sum (num) {
-    let parameter = Math.abs(Math.floor(num));
+    let parameter = Math.floor(Math.abs(num));
     let result = 0;
     if (num == 0) {
         result = 0;
@@ -13,12 +13,11 @@ function sum (num) {
     }
     
 }
+const sum2 = (num) => {let param = Math.abs(Math.floor(num));let result=(1 + param)*param/2;return result;}
 
-console.log(1) // 1;
-console.log(2) // 3;
-console.log(3) // 6;
-
+console.log(sum2(1));
+console.log(sum2(10));
 console.log(sum(0));
-console.log(sum(1));
-console.log(sum(10));
-console.log(sum(-3));
+console.log(sum(-5));
+console.log(sum(2.5));
+console.log(sum(-7.2));
