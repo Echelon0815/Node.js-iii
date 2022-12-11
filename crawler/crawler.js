39994a3d7bc35d0d6c6a1bd8121c4fe1724d9652
Echelon0815 <jack.w886@gmail.com>
2022-12-11 15:30:35 +0800

    crawler.js

---
+++ crawler/crawler.js
@@ -0,0 +1,26 @@
+//http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
+
+const  axios = require('axios');
+
+// axios
+//     .get('http://54.71.133.152:3000/stocks?stockNo=2618&date=202211')
+//     .then((response) => {
+//         console.log(response.data);
+//     })
+//     .catch((e) => {
+//         console.error(e);
+//     });
+
+
+
+async function getData() {
+    try {
+        const get = await axios.get('http://54.71.133.152:3000/stocks?stockNo=2618&date=202211');
+        const data = get;
+        console.log(data);
+    }   catch (error) {
+        throw new Error(error);
+    }
+}
+
+getData();
